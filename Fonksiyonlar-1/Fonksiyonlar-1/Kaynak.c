f1155c2158c495e1d58c42c7f338166e6933967d
ErayMersin <116799603+ErayMersin@users.noreply.github.com>
2022-11-20 19:08:36 +0300

    Fonksiyon

---
+++ Fonksiyonlar-1/Fonksiyonlar-1/Kaynak.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void listele() {
+	printf("Mehmet Yilmaz\n");
+	printf("Ali Turk\n");
+	printf("Kemal Sonmez\n");
+	printf("Ayse Gormez\n");
+	printf("Gorkem Atar\n");
+	printf("Selin Bakar\n");
+ }
+int main() {
+
+	listele();
+
+	return 0;
+   }
